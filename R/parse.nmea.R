158ac4f1036eb97c0554a0829f1f724c60bbc89b
Hector Villalobos <h.villalobos@protonmail.com>
2021-03-24 14:59:46 -0700

    parse.nmea fixed to process GPRMC

--- R/parse.nmea.R
+++ R/parse.nmea.R
@@ -6,20 +6,25 @@
 #' as returned by \code{get_NME0}.
 #' 
 #' @param sentence A string identifying the NMEA sentence to process. It
-#' can be one of "GPGGA", "INGGA", "GPGLL", "GPRMC".
+#' can be one of "GPRMC", "GPGGA", "INGGA", or "GPGLL".
 #'
 #' @param returnAll logical. If \code{TRUE}, all data found in the selected
 #' sentence is returned.
 #'
-#' @details This function identifies among the sentences found in the data, 
-#' the one with more information (more GPS fixes). However, sometimes they 
-#' may be corrupted, so the user can choose a different sentence to process.
+#' @details This function looks first for the "GPRMC" sentence, if not found  
+#' it tries then with "GPGGA", "INGGA", or "GPGLL", and process the one 
+#' more information (more GPS fixes). Be ware that sometimes strings may be
+#' corrupted, so it is possible to choose manually the sentence to process.
 #' This is why \code{get_NME0} does not parse the NMEA strings by default. 
-#' If sentences: "GPVTG", "INVTG", "GNVTG", or "IIVTG" are found, vessel 
-#' speed and bearing are also parsed and returned.
+#' When one of "GPVTG", "INVTG", "GNVTG", or "IIVTG" sentences is found, vessel 
+#' speed and bearing are also parsed and returned. 
+#' Differences in time.cpu and gps.time may reveal wrong clock settings in the 
+#' data aquisition computer.
 #'
-#' @return A data frame with cpu time, gps time, miliseconds, longitude, and
-#' latitude. Also, vessel speed and bearing if found in the data.
+#' @return A data frame with cpu time, gps time, miliseconds (already added
+#' in gps time when the chossen sentence is "GPRMC"), longitude, and
+#' latitude. Additionally, vessel speed and bearing are returned if found in
+#' the data.
 #'
 #' @author Héctor Villalobos.   
 #'
@@ -37,65 +42,71 @@
 #' }
 parse.nmea <- function(nmea, sentence = NULL, returnAll = FALSE)
 {
-  #datetime <- paste(substr(date.real, 1, 10), GPS$time.gps)
-  #GPS$time.gps <- strptime(datetime, format = "%Y-%m-%d %H:%M:%S", tz = "UTC")
-  #dt <- date.real - GPS$time.gps[1] 
-  #if (dt != 0)
-  #  warning("cpu time and gps time mismatch. Time difference is ", 
-  #          round(dt, 2), " ", attr(dt, "units"))
-  
-  
-  # Available sentences in data
+  # Available sentences in the data
   sentences <- substr(nmea[, 2], 2, 6)
   # Sentences with position data. More than one may be present.
-  poss <- c("GPGGA", "INGGA", "GPGLL", "GPRMC")
+  swp <- c("GPRMC", "GPGGA", "INGGA", "GPGLL")
   # columns' indices
-  idx <- list(GPGGA = 2:6, INGGA = 2:6, GPGLL = c(6, 2:5), GPRMC = c(2, 4:7))  
+  idx <- list(GPRMC = c(2, 4:7, 10), GPGGA = 2:6, INGGA = 2:6, GPGLL = c(6, 2:5))  
   # find out which are present
-  px <- poss %in% sentences
-  poss <- poss[px]
+  px <- swp %in% sentences
+  swp <- swp[px]
   
-  # If manual selection
+  # Manual selection
   if (!missing(sentence)){
-    if(!sentence %in% poss)
+    if(!sentence %in% swp)
       stop ("NMEA sentence ", sentence, " not found in data.", 
-      "\n It should be one of: ", paste(poss, collapse = ", "))
+      "\n It should be one of: ", paste(swp, collapse = ", "))
     at <- grep(sentence, sentences)
     idx <- idx[[sentence]]
-  } else {
-    idx <- idx[px]
-    ns <- length(poss)
-    for ( j in 1:ns ){
-      at <- grep(poss[j], sentences)
-      len <- length(at)
-      assign(paste("at", j, sep="."), at)
-      assign(paste("len", j, sep="."), len)
-    }  
-    # keep the one with more data, if equal, take the first
-    px <- mget(paste("len", 1:ns, sep="."))
-    ix <- which.max(px)
-    at <- grep(poss[ix], sentences) 
-    idx <- idx[[ix]]
+  } else { 
+    if ("GPRMC" %in% swp){ # try GPRMC
+      at <- grep("GPRMC", sentences)
+      idx <- idx[["GPRMC"]]
+    } else { # or one of the remaining...
+      ns <- length(swp)
+      for ( j in 1:ns ){
+        at <- grep(swp[j], sentences)
+        len <- length(at)
+        assign(paste("at", j, sep="."), at)
+        assign(paste("len", j, sep="."), len)
+      }
+      # process the one with more data, if equal, take the first
+      px <- mget(paste("len", 1:ns, sep="."))
+      ix <- which.max(px)
+      at <- grep(swp[ix], sentences) 
+      idx <- idx[[ix]]
+    }
   }  
   GPSs <- nmea[at, 2]
   dgTime <- nmea[at, 'dgTime']
   nc <- length(unlist(strsplit(GPSs[1], split = ",")))
   spl <- unlist(strsplit(GPSs, split = ","))
   GPSs <- as.data.frame(matrix(spl, ncol = nc, byrow = TRUE))
-    
+  
   # Time
   ti <- GPSs[, idx[1]]
   nchar.ti <- unlist(lapply(ti, nchar))
-  time <- paste(substr(ti, 1, 2), substr(ti, 3, 4), substr(ti, 5, 6), sep=":")
   
-  # there are miliseconds? Normally present in GGA, but not in GLL nor RMC
+  # there are miliseconds? 
   if (all(nchar.ti - 9 == 0)){
     ms <- as.numeric(ti) %% 1
   } else {
     ms <- rep(0, length(ti))
   }
+  # add ms to seconds
+  sec <- paste(substr(ti, 5, 6), ms, sep = ".")
+  # create time string
+  time <- paste(substr(ti, 1, 2), substr(ti, 3, 4), sec, sep=":")
   
-    
+  # Date, when "GPRMC" is present
+  if(length(idx) > 5){
+    da <- GPSs[, idx[6]]
+    date <- paste(substr(da, 1, 2), substr(da, 3, 4), substr(da, 5, 6), sep="/")
+    DT <- paste(date, time)
+    time <- strptime(DT, format = "%d/%m/%y %H:%M:%OS", tz = "UTC")
+  }
+      
   # Decimal Longitudes and Latitudes     
   lat <- as.numeric(GPSs[, idx[2]])/100
   lat <- floor(lat) + (lat %% 1) * 10/6
@@ -111,14 +122,13 @@ parse.nmea <- function(nmea, sentence = NULL, returnAll = FALSE)
   if (length(idxW) > 0 )
     lon[idxW] <- -lon[idxW]
 
-  ans <- data.frame(dgTime = dgTime, time.gps = time, ms = ms, lon = lon, lat = lat)
+  ans <- data.frame(time.cpu = dgTime, time.gps = time, ms = ms, lon = lon, lat = lat)
   nr <- nrow(ans)
   
   # If vessel speed and bearing are found 
   # Possible sentences identified so far: 
   vs <- c("GPVTG", "INVTG", "GNVTG", "IIVTG")
   # can't filter just by "VTG" because some files have data from two different VTG sentences
-  # find which are present
   vs <- vs[vs %in% sentences]
   ns <- length(vs)
   lens <- rep(NA, ns)
